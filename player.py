2e6d5893d08801cbd533c84631b84cbb80618be4
Ed Knueppel <edward.knueppel@zywave.com>
2021-11-27 18:52:56 -0600

    add player class

---
+++ player.py
@@ -0,0 +1,7 @@
+class Player:
+    def __init__(self, name: str, age: int, gender: str, country:str, rating:float) -> None:
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.country = country
+        self.rating = rating
