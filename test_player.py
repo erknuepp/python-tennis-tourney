678937c186419c16b1c4a9752f183f75b3ca2aff
Ed Knueppel <edward.knueppel@zywave.com>
2021-11-27 23:22:21 -0600

    adds io test

--- test_player.py
+++ test_player.py
@@ -1,7 +1,16 @@
+import json
 import player
 import unittest
 
 class Test_Player(unittest.TestCase):
+
+    def test_io(self):
+        f = open('players.json')
+        players = json.load(f)
+        f.close()
+        self.assertIsNotNone(players)
+
+        
 
     def test_initialize(self):
         expectedName = "Ed"
